701ea5b548ac69839d99196baff7c584f8120fb7
borislavml <borislav.irdsp@gmail.com>
2018-07-31 17:24:20 +0300

    - initial commit

---
+++ Maps/Pages/Index.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Maps.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public IConfiguration Configuration { get; }
+        public string MapboxAccessToken { get; }
+
+        public IndexModel(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            MapboxAccessToken = Configuration["Mapbox:AccessToken"];
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnGetAirports()
+        {
+            var configuration = new Configuration
+            {
+                BadDataFound = context => { }
+            };
+
+            using (var sr = new StreamReader(Path.Combine(_hostingEnvironment.WebRootPath, "airports.dat")))
+            using (var reader = new CsvReader(sr, configuration))
+            {
+                FeatureCollection featureCollection = new FeatureCollection();
+
+                while (reader.Read())
+                {
+                    string name = reader.GetField<string>(1);
+                    string iataCode = reader.GetField<string>(4);
+                    double latitude = reader.GetField<double>(6);
+                    double longitude = reader.GetField<double>(7);
+
+                    featureCollection.Features.Add(new Feature(
+                        new Point(new Position(latitude, longitude)),
+                        new Dictionary<string, object>
+                        {
+                            { "name", name },
+                            { "iataCode" , iataCode }
+                        }));
+                }
+
+                return new JsonResult(featureCollection);
+            }
+        }
+    }
+}
